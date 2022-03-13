809a5b36005f24923b6f8ca7957136f3328c163c
Kris Raven <kris.raven@zoho.com>
2022-03-13 21:07:34 +1100

    feat: add examples of design Patterns

---
+++ patterns/singleton.go
@@ -0,0 +1,20 @@
+// creational design pattern
+
+package singleton
+
+import (
+	"sync"
+)
+
+type singleton struct {
+}
+
+var instance *singleton
+var once sync.Once
+
+func GetInstance() *singleton {
+	once.Do(func() {
+		instance = &singleton{}
+	})
+	return instance
+}
