c71beff98db64366f57b2a66a573da824acea0dd
Kris Raven <krisraven@users.noreply.github.com>
2023-09-05 21:49:28 +1000

    Update memoryAddresses.go

--- gobyexample/memoryAddresses.go
+++ gobyexample/memoryAddresses.go
@@ -8,7 +8,8 @@ func main() {
 	fmt.Println(a) // "hello world"
 	fmt.Println(&a) // 0xc000017400
 
-	var b *string = &a
+	var b *string = &a // whatevr is assigned to b must be a point string
 	fmt.Println(b) // 0xc000017400
 	fmt.Println(*b) // "hello world"
+	fmt.Println(&b) // 0xc000084280
 }
